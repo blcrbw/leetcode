df6ff09865d0758da6ae7fb855a10dfb174f11bd
blc <blc@blct@local.home>
2025-09-28 23:46:29 +0700

    111: dfs.

---
+++ 111/main.go
@@ -0,0 +1,32 @@
+package main
+
+// 111. Minimum Depth of Binary Tree
+// Given a binary tree, find its minimum depth.
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+// Note: A leaf is a node with no children.
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+func min(a, b int) int {
+	if a == 0 {
+		return b
+	}
+	if b == 0 {
+		return a
+	}
+	if a > b {
+		return b
+	}
+	return a
+}
